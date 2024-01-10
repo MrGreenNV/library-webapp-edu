3bf92f85ece994211473cd92789fa8d312b4e457
MrGreenNV <averkievnv@gmail.com>
2024-01-10 21:34:57 +0300

    updated Role add AuthorRegistrationDTO, ReaderRegistrationDTO

--- src/main/java/ru/miit/webapp/models/Role.java
+++ src/main/java/ru/miit/webapp/models/Role.java
@@ -2,26 +2,18 @@ package ru.miit.webapp.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "roles")
+@NoArgsConstructor
 public class Role extends BaseEntity {
     private UserRoles name;
-
     public Role(UserRoles name) {
-        this.name = name;
-    }
-
-
-    public Role() {
-
-    }
-
-    public UserRoles getName() {
-        return this.name;
-    }
-
-    public void setName(UserRoles name) {
         this.name = name;
     }
 }
---
+++ src/main/java/ru/miit/webapp/models/dto/ReaderRegistrationDTO.java
@@ -0,0 +1,32 @@
+package ru.miit.webapp.models.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class ReaderRegistrationDTO {
+    @NotEmpty(message = "Поле не должно быть пустым.")
+    @Size(min = 3, max = 255, message = "В имени должно быть от 3 до 255 символов.")
+    private String name;
+    @NotEmpty(message = "Поле не должно быть пустым.")
+    @Size(max = 255, message = "Email не должен содержать больше 255 символов.")
+    @Email(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+.+.[A-Za-z]{2,4}$", message = "Email должен быть валиден.")
+    private String email;
+
+    @NotNull(message = "Поле не должно быть пустым.")
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dateOfBirth;
+    private String password;
+}
